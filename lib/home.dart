364309822fe4aa2ead4b9c7313d895634c6ddb47
anasahmed01 <zainkhilji2323@gmail.com>
2022-09-25 13:21:18 +0500

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Homeapp extends StatefulWidget {
+  const Homeapp({super.key});
+
+  @override
+  State<Homeapp> createState() => _HomeappState();
+}
+
+class _HomeappState extends State<Homeapp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
