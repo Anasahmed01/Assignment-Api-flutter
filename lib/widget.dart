a94c7f008a432516ced283eb2fd4715c72618c0f
anasahmed01 <zainkhilji2323@gmail.com>
2022-10-02 17:42:20 +0500

    add

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
 
+showAlertDialog(
+  BuildContext context,
+  removeFunction,
+) {
+  // Create button
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      removeFunction;
+      Navigator.of(context).pop();
+    },
+  );
+  Widget cancleButton = TextButton(
+    child: const Text("Cancel"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  // Create AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: const Text("Alert!"),
+    content: const Text("Are you sure Delete this "),
+    actions: [
+      okButton,
+      cancleButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
